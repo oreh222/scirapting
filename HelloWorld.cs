bbc695074d39d79472bbfc5d92f201048de4337f
oreh222 <maks.okhotin@gmail.com>
2025-02-01 12:29:20 +0300

    no changas, dona't thinak that there is changis

---
+++ HelloWorld.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Helalo Worald");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
